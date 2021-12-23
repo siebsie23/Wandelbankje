065766433c12f4cd3197b656702e0e49231761d8
Sibren <sibrenvansetten@hotmail.nl>
2021-12-23 14:25:16 +0100

    Added current location marker

--- public/js/benches_yourarea.js
+++ public/js/benches_yourarea.js
@@ -15,6 +15,16 @@ $(document).ready(function() {
             zoomOffset: -1,
             accessToken: 'sk.eyJ1Ijoic2llYnNpZTIzIiwiYSI6ImNreDRmeXRlajI5ajQybnA4cWpzcWNoMnIifQ.vMunAm-ZN4Qq5hZ-4153CA'
         }).addTo(map);
+        var redIcon = new L.Icon({
+            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
+        let yourLocationMarker = L.marker([position.latitude, position.longitude], {icon: redIcon}).addTo(map);
+        yourLocationMarker.bindPopup('Je huidige locatie');
         markers.addTo(map);
         last_position = position;
         updateMarkers(position);
