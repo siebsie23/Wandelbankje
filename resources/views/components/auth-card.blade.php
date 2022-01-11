00f4a78caa7bd68849e80943d80cf2d6cb6c5bfa
Sibren <sibrenvansetten@hotmail.nl>
2022-01-11 11:36:26 +0100

    Updated auth card and button

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center bg-gray-100">
+<div class="mt-20 flex flex-col sm:justify-center items-center bg-gray-100">
     <div class="w-full sm:max-w-md px-6 py-2 bg-white shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
