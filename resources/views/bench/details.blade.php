ff9ffef198e1c446eee97f794f53a3c5615f713d
Sibren <sibrenvansetten@hotmail.nl>
2022-01-20 13:26:46 +0100

    Fixed the carousel being frozen if there are multiple images

--- resources/views/bench/details.blade.php
+++ resources/views/bench/details.blade.php
@@ -18,15 +18,21 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     @php
                         $hasPhotos = false;
+                        $it = 0;
                     @endphp
 
                     <div id="carouselExampleControls" class="carousel slide relative" data-bs-ride="carousel">
                         <div class="carousel-inner relative w-full overflow-hidden">
                             @foreach(\App\Models\Photo::where('bench', $bench->id)->get() as $photo)
                                 @php
-                                    $hasPhotos = true
+                                    $hasPhotos = true;
+                                    if($it == 0)
+                                        $active = 'active ';
+                                    else
+                                        $active = '';
+                                    $it++;
                                 @endphp
-                                <div class="carousel-item active relative float-left w-full">
+                                <div class="carousel-item {{ $active }}relative float-left w-full">
                                     <img
                                         src="{{ asset('images/benches/' . $photo->path) }}"
                                         class="block w-full object-cover"
@@ -97,7 +103,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                             </svg>
                         </a>
-                        <a href="#" class="ml-1 mr-1 p-2 pl-5 pr-5 transition-colors duration-700 transform bg-indigo-500 hover:bg-blue-400 text-gray-100 text-lg rounded-lg focus:border-4 border-indigo-300 flex justify-center">
+                        <a href="{{ route('bench.addphoto', $bench->id) }}" class="ml-1 mr-1 p-2 pl-5 pr-5 transition-colors duration-700 transform bg-indigo-500 hover:bg-blue-400 text-gray-100 text-lg rounded-lg focus:border-4 border-indigo-300 flex justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
